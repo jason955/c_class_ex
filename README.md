6a6598e3f11e50f72169ffb3d6296749674229ad
JASON <jason@jason.com>
2022-10-20 16:06:49 -0400

    mutex

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Compile with:
 
 gcc test.cpp date.cpp -o date
+
+
+g++ -pthread -o mutex mutex.cc
+
--- mutex.cc
+++ mutex.cc
@@ -11,25 +11,26 @@ std::mutex m;
 
 struct X {
 	void foo(int i, const std::string& str) {
-		std::lock_guard<std::mutex> lk(m)
+		std::lock_guard<std::mutex> lk(m);
 		std::cout << str << ' ' << i << '\n';
 	}
 	
 	void bar(const std::string& str) {
-		std::lock_guard<std::mutex> lk(m)
-		std::cout << str << \n';
+		std::lock_guard<std::mutex> lk(m);
+		std::cout << str << "\n";
 	}
 	
-	void operator(int i) {
-		std::lock_guard<std::mutex> lk(m)
-		std::cout << str << ' ' << i << '\n';
+	int operator()(int i) {
+		std::lock_guard<std::mutex> lk(m);
+		std::cout<< i << '\n';
+		return i + 10;
 	}
-}
+};
 
 template <typename RandomIt>
 int parallel_sum(RandomIt beg, RandomIt end)
 {
-	auto len = end - beg
+	auto len = end - beg;
 	if (len < 1000)
 	{
 		std::accumulate(beg, end, 0);
@@ -46,13 +47,14 @@ int parallel_sum(RandomIt beg, RandomIt end)
 int main (int argc, char *argv[])
 {
 	std::vector<int> v(10000, 1);
-	std::cout << " The sum is " << parallel_sum(v.begin(), v.end()) << '\n';
+	std::cout << "YEEHAW";
+	//std::cout << " The sum is " << parallel_sum(v.begin(), v.end()) << '\n';
 	
 	X x;
 	
 	auto a1 = std::async(&X::foo, &x, 42, "Hello");
 	
-	auto a2 = std::async(std::launch::deferred, &X::bar, x "world!");
+	auto a2 = std::async(std::launch::deferred, &X::bar, x, "world!");
 	
 	auto a3 = std::async(std::launch::async, X(), 43);
 	
